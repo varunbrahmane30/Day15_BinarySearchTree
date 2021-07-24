627903a0ce8573b273ef18d14427cabead91a83a
name <varunbrahmane30@gmail.com>
2021-07-24 13:52:20 +0530

    [varun] added: binary search tree add [56 -30 - 70]

--- Program.cs
+++ Program.cs
@@ -7,6 +7,15 @@ namespace Day15_BinarySearchTree
         static void Main(string[] args)
         {
             Console.WriteLine(" Binary Search Tree ");
+            
+            // adding 56 into Bsst with initial value 56 as ROOT
+            BinarySearchTree<int> binarySearch = new BinarySearchTree<int>(56);
+
+            // adding 30 - 70 into BST
+            binarySearch.Insert(30);
+            binarySearch.Insert(70);
+
+            binarySearch.Display();
         }
     }
 }
