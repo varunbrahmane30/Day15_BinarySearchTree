fa62ab53914a95df3d72f0cc4270f12a758e6b42
name <varunbrahmane30@gmail.com>
2021-07-24 13:32:42 +0530

    [varun] added: binary search tree main program

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Day15_BinarySearchTree
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(" Binary Search Tree ");
+        }
+    }
+}
